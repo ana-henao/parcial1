7812fbd33d66e80ae2ed5614348d322b671837ac
Ana Henao <ana.henao8@udea.edu.co>
2020-10-17 16:20:21 -0500

    funcion lanzamiento de defensa

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 using namespace std;
 void lanzamientoEnemigo(float *punto, float Voy, float Vox,float g, float *tfinal);
+void lanzamientoDefensa(float *punto, float Voy, float Vox, float angulo, float g, float *tfinal, float *puntoD);
 
 int main()
 {   float g=9.81;
@@ -20,14 +21,17 @@ int main()
     float punto[2]={0,Ho};//valor inicial de la posición del proyectil enemigo
     float tfinal=0;
 
+    lanzamientoEnemigo(&punto[0], Ovoy, Ovox, g, &tfinal);
 
+
+    return 0;
 
 
 
 
 
 }
-void lanzamientoEnemigo(float *punto, float Voy, float Vox,float g, float *tfinal){
+void lanzamientoEnemigo(float *punto, float Voy, float Vox,float g, float *tfinal, float *puntoD){
 
     float Vy=Voy;//Velocidad inicial y
     float Vx=Vox;
@@ -35,23 +39,53 @@ void lanzamientoEnemigo(float *punto, float Voy, float Vox,float g, float *tfina
     float X=0;
 
     float t=0;
-    while(Vy>0){
+    while(Y>=0){//Mientras Y sea mayor a 0 es decir no este tocando el suelo
 
-        Y=*(punto+1)+(Vy*t)-((1/2)*g*(pow(t,2)));
-        Vy=Vy-(g*t);
-        cout<<t<<endl;
+        Y=*(punto+1)+(Vy*t)-((1/2)*g*(pow(t,2)));//Posición en Y
+        Vy=Vy-(g*t);//Velocidad Y
+        cout<<Vy<<endl;
 
-        X=*(punto)+(Vx*t);
+        X=*(punto)+(Vx*t);//Posicion en X
 
-
-        *punto=X;
-        *(punto+1)=Y;
-
-        cout<<X<<" "<<Y<<endl;
-        t+=0.01;
-        *tfinal=t;
+        if (X<=*puntoD + 0.05 && Y<=*(puntoD+1) + 0.05){
+            }
     }
+void lanzamientoDefensa(float *punto, float Voy, float Vox, float angulo, float g, float *tfinal, float *puntoD){
+
+            float Vy=Voy;//Velocidad inicial y
+            float Vx=Vox;
+            float Y=0;
+            float X=0;
+
+            float Yini=*punto;
+            float Xini=*(punto+1);
+
+            float t=0;
+            while(Y>=0){//Mientras Y sea mayor a 0 es decir no este tocando el suelo
+
+                Y=*(punto+1)+(Vy*t)-((1/2)*g*(pow(t,2)));//Posición en Y
+                Vy=Vy-(g*t);//Velocidad Y
+                cout<<Vy<<endl;
+
+                X=*(punto)+(Vx*t);//Posicion en X
+
+                if (X<=*puntoD + 0.05 && Y<=*(puntoD+1) + 0.05){
+
+                    cout<<"Impacto"<<endl;
+                    infoImpactoCaso1(Xini, Yini, Voy, Vox, angulo);
+                    break;
+
+                }else{
+
+                    *punto=X;
+                    *(punto+1)=Y;
+
+                    //cout<<X<<" "<<Y<<endl;
+                    t-=0.0001;
+                    *tfinal=t;
+                }
 
 
-}
+            }
 
+        }
